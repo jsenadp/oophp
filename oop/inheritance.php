c94bd4b85843e6165e1559a82ec64bbb944f9fd5
jsenadp <56987482+jsenadp@users.noreply.github.com>
2022-08-07 17:51:33 +0700

    Add files via upload

---
+++ oop/inheritance.php
@@ -0,0 +1,65 @@
+
+<?php
+class Produk{
+    public $judul,
+           $penulis,
+           $penerbit,
+           $harga,
+           $jmlHalaman,
+           $jamMain;
+    
+    public function __construct($judul="judul", $penulis="penulis", $penerbit="penerbit", $harga=0, $jmlHalaman=0, $jamMain=0,)
+    {
+        $this->judul=$judul;
+        $this->penulis=$penulis;
+        $this->penerbit=$penerbit;
+        $this->harga=$harga;
+        $this->jmlHalaman=$jmlHalaman;
+        $this->jamMain=$jamMain;
+    }
+
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+    }
+    //Buku : PHP | Sandika, Gramedia (Rp. 20000) - 100 Halaman
+    //Game : Mobile Legend | Montoon, Playstore (Rp. 300000) ~ 50 Jam
+    public function getInfoProduk(){
+        // $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. $this->harga) ";
+        // if ($this->tipe == "Buku") {
+        //     $str .="- {$this->jmlHalaman} Halaman.";
+        // } else  {
+        //     $str .="~ {$this->jamMain} Jam.";
+        // }
+        // return $str;
+    }
+}
+
+class Buku extends Produk{
+    public function getInfoProduk(){
+        $str = "Buku : {$this->judul} | {$this->getLabel()} (Rp. $this->harga) - {$this->jmlHalaman}";
+        return $str;
+    }
+}
+
+class Game extends Produk{
+    public function getInfoProduk(){
+        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. $this->harga) ~ {$this->jamMain}";
+        return $str;
+    }
+}
+
+class cetakInfoProduk{
+    public function cetak($produk){
+        $str = "Judul : {$produk->judul} | Penulis : {$produk->penulis}, Penerbit : {$produk->penerbit}, Harga : {$produk->harga}";
+        return $str;
+    }
+}
+
+$produk1 = new Buku("PHP", "Sandika", "Gramedia", 20000, 100, 0);
+$produk2 = new Game("Mobile Legend", "Montoon", "Playstore", 300000,0, 50);
+
+//Buku : PHP | Sandika, Gramedia (Rp. 20000) - 100 Halaman
+//Game : Mobile Legend | Montoon, Playstore (Rp. 300000) ~ 50 Jam
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
---
+++ oop/overriding.php
@@ -0,0 +1,81 @@
+
+<?php
+class Produk{
+    public $judul,
+           $penulis,
+           $penerbit,
+           $harga;
+    
+    public function __construct($judul="judul", $penulis="penulis", $penerbit="penerbit", $harga=0)
+    {
+        $this->judul=$judul;
+        $this->penulis=$penulis;
+        $this->penerbit=$penerbit;
+        $this->harga=$harga;
+    }
+
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+    }
+    //Buku : PHP | Sandika, Gramedia (Rp. 20000) - 100 Halaman
+    //Game : Mobile Legend | Montoon, Playstore (Rp. 300000) ~ 50 Jam
+    public function getInfoProduk(){
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. $this->harga) ";
+        // if ($this->tipe == "Buku") {
+        //     $str .="- {$this->jmlHalaman} Halaman.";
+        // } else  {
+        //     $str .="~ {$this->jamMain} Jam.";
+        // }
+        return $str;
+    }
+}
+
+class Buku extends Produk{
+    public $jmlHalaman;
+
+    public function __construct($judul="judul", $penulis="penulis", $penerbit="penerbit", $harga=0, $jmlHalaman=0){
+
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+    
+        $this->jmlHalaman = $jmlHalaman;
+
+    }
+
+    public function getInfoProduk(){
+        $str = "Buku : ".parent::getInfoProduk()." - {$this->jmlHalaman}";
+        return $str;
+    }
+}
+
+class Game extends Produk{
+    public $jamMain;
+
+    public function __construct($judul="judul", $penulis="penulis", $penerbit="penerbit", $harga=0, $jamMain=0){
+        
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+
+        $this->jamMain = $jamMain;
+
+    }
+
+    public function getInfoProduk(){
+        $str = "Game : ".parent::getInfoProduk()." ~ {$this->jamMain}";
+        return $str;
+    }
+}
+
+class cetakInfoProduk{
+    public function cetak($produk){
+        $str = "Judul : {$produk->judul} | Penulis : {$produk->penulis}, Penerbit : {$produk->penerbit}, Harga : {$produk->harga}";
+        return $str;
+    }
+}
+
+$produk1 = new Buku("PHP", "Sandika", "Gramedia", 20000, 100);
+$produk2 = new Game("Mobile Legend", "Montoon", "Playstore", 300000, 50);
+
+//Buku : PHP | Sandika, Gramedia (Rp. 20000) - 100 Halaman
+//Game : Mobile Legend | Montoon, Playstore (Rp. 300000) ~ 50 Jam
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
